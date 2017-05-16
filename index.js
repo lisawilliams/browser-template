4ed444a5a3ecd8c8f85e4528cdf188238f2628c1
lisawilliams <lisa@placeblogger.com>
2017-05-15 20:36:14 -0400

    Takes modal form input and logs it to the console.

--- index.js
+++ index.js
@@ -5,7 +5,25 @@
 
 // load manifests
 // scripts
-require('./assets/scripts/index.js')
+// require('./assets/scripts/index.js')
+
+const config = require('./config')
+
+$(() => {
+  setAPIOrigin(location, config)
+  $('form').on('submit', function (event) {
+    event.preventDefault()
+    const input = $('#name').val()
+    console.log(input)
+    $('#myModal').modal('hide')
+  })
+})
+
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example');
 
 // styles
-require('./assets/styles/index.scss')
+// require('./assets/styles/index.scss')
+
+// styles
+// require('./assets/styles/index.scss')
